35eeefaff9d47bb7275f0fe85026b4008386c75e
jesus <jesus@gmail.com>
2016-05-20 11:47:28 +0200

    index php

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php
+include('lib/scrolltop.php');
+include('lib/animation.php');
+?>
+<!--DOCTYPE html -->
+<html><head>
+    <meta charset="utf-8">
+    <title>TWISTER Traffic</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/images/TWISTER-FAVICON.ico">
+    <!-- Loading Bootstrap -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-contact.css" rel="stylesheet">
+    <link href="css/style-content.css" rel="stylesheet">
+    <link href="css/style-footers.css" rel="stylesheet">
+    <link href="css/style-headers.css" rel="stylesheet">
+    <link href="css/style-portfolios.css" rel="stylesheet">
+    <link href="css/style-pricing.css" rel="stylesheet">
+    <link href="css/style-team.css" rel="stylesheet">
+    <link href="css/style-dividers.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<div id="page" class="page">
+    <!-- /.wrapper -->
+    <!-- /.item --><div class="item header margin-top-0 padding-bottom-0">
+        <header class="wrapper image image2">
+            <div class="bg bg2"></div>
+            <div class="container">
+                <nav role="navigation" class="navbar navbar-inverse navbar-grey navbar-none">
+                    <div class="navbar-header">
+                        <button data-target="#navbar-collapse-02" data-toggle="collapse" class="navbar-toggle" type="button">
+                            <span class="sr-only">Toggle navigation</span>
+                        </button>
+                        <a href="#home" class="navbar-brand brand"><img alt="" src="images/icons/loop.svg" style="height: 50px;" id="shake"> <b>TWISTER</b> Traffic</a>
+                    </div>
+                    <div id="navbar-collapse-02" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active propClone"><a href="#home">Home</a></li>
+                            <li class="propClone"><a href="#work">Work</a></li>
+                            <li class="propClone"><a href="#contacto">Contacto</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </nav>
+
+                <a name="home"></a>
+                <div class="row banner2">
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <h1>Tráfico gratuito para tu sitio Web.
+                        </h1>
+                        <h3 style="font-weight: normal;">
+                            Escalable, resultados para webmasters bajo demanda.
+                        </h3>
+                        <h3 style="font-weight: normal;">
+                            Regístrate ahora para conseguir 1000 puntos de tráfico GRATIS!
+                        </h3>
+                        <br>
+                        <a href="content/login1.php" class="btn btn-danger btn-hg btn-wide"><span class="fa fa-sign-in"></span> Ingresa</a>
+                        <a href="content/Registro.php" class="btn btn-success btn-hg btn-wide"><span class="fa fa-sign-in"></span> Registro</a>
+                    </div>
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </header>
+    </div><!-- /.wrapper -->
+
+    <a name="work"></a>
+    <!-- /.item --><div class="item content dark padding-bottom-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <div id="pulse">
+                    <img alt="" src="images/icons/search.svg"></div>
+                    <h3>Visitas Reales</h3>
+                    <p>
+                        Trafico real, rastreable por Google Analytics.
+                    </p>
+                    <p>
+                        Trafico desde Facebook, Twitter, Linkedin y Google.
+                    </p>
+                    <a href="content/Registro.php" class="btn btn-success btn-hg btn-wide"><span class="fa fa-sign-in"></span> Registro</a><br><br>
+                </div>
+                <!-- /.col-md-4 col -->
+                <div class="col-md-4 text-center">
+                    <div id="bounce">
+                    <img alt="" src="images/icons/graph.svg"></div>
+                    <h3>Impulsa tu ranking Alexa</h3>
+                    <p>
+                        El ranking de Alexa muestra el tráfico de un sitio web.
+                    </p>
+                    <p>
+                        Cuanto menor sea tu rango, mas visitas tienes!
+                    </p>
+                    <a href="content/Registro.php" class="btn btn-success btn-hg btn-wide"><span class="fa fa-sign-in"></span> Registro</a><br><br>
+                </div><!-- /.col-md-4 col -->
+                <div class="col-md-4 text-center">
+                    <div id="double">
+                    <img alt="" src="images/icons/money.svg"></div>
+                    <h3>Aumenta el Valor de tu Web</h3>
+                    <p>
+                        Conseguir visitantes aportará más valor a tu web.
+                    </p>
+                    <p>
+                        Tu sitio web podría ser visto hasta 50 veces más!
+                    </p>
+                    <a href="content/Registro.php" class="btn btn-success btn-hg btn-wide"><span class="fa fa-sign-in"></span> Registro</a><br><br>
+                </div><!-- /.col-md-4 col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.item -->
+
+    <div class="footerWrapper">
+        <a name="contacto"></a>
+        <div class="item footer dark">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center social">
+                        <h2><img alt="" src="images/icons/loop.svg" style="height: 50px;"> <b>TWISTER</b> Traffic</h2>
+                        <span>Siguenos en:</span><br>
+                        <a href="https://www.facebook.com/ganardineronline.es/"><span class="fa fa-facebook-square"></span></a>
+                        <a href="https://twitter.com/chussanm"><span class="fa fa-twitter-square"></span></a>
+                        <a href="https://es.linkedin.com/in/chusantana"><span class="fa fa-linkedin-square"></span></a>
+                        <a href="https://plus.google.com/u/0/+JesusSantana/"><span class="fa fa-google-plus-square"></span></a>
+                        <a href="https://es.pinterest.com/grial101/"><span class="fa fa-pinterest-square"></span></a>
+                        <a href="content/soporte.php"><span class="fa fa-envelope-square"></span></a>
+                        <br><div align="center"><i class="fa fa-copyright fa-fw"></i>2016 <a href='http://www.esmarketingdigital.es'><b>esMarketingDigital</b></a></div>
+                        <a class="go-top" href="#"><i class="fa fa-angle-up fa-fw fa-3x"></i></a>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div>
+        </div><!-- /.item -->
+    </div><!-- /.footerWrapper -->
+</div><!-- /#page -->
+
+
+<!-- Load JS here for greater good =============================-->
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+<script src="js/jquery.ui.touch-punch.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-select.js"></script>
+<script src="js/bootstrap-switch.js"></script>
+<script src="js/flatui-checkbox.js"></script>
+<script src="js/flatui-radio.js"></script>
+<script src="js/jquery.tagsinput.js"></script>
+<script src="js/jquery.placeholder.js"></script>
+<script src="js/application.js"></script>
+
+</body></html>
